4cd008f1306e20009aa894409b8b90ea1e4be976
Khadijah Akter <khadijahakter123@gmail.com>
2023-06-06 00:39:18 -0400

    Completed the titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,9 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  const arr = title.split(' '); // separate each word in the string
+  const capital = arr.map(word => word[0].toUpperCase() + word.substring(1)); // capitalize the first letter and combine with the rest of the letters
+  const titleCase = capital.join(' '); // combine each word into a single string again
+  return titleCase;
 }
 
 // Do not edit this line;
